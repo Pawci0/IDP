040fe3be0e110928d8e8e48d9fad64159cbacdc7
Aleksandra Kowalczyk <aleksandra.kowalczyk.0019@gmail.com>
2020-10-26 22:23:19 +0100

    Add mean square error calculating, refactor code

--- Task1/main.py
+++ Task1/main.py
@@ -1,41 +1,73 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 import json
 
-with open('config/config.json') as configFile:
-    config = json.load(configFile)
+sb.set_theme(style="darkgrid")
 
-epochs = config['epochs']
-trainingStep = config['trainingStep']
-wBoundLower = config['wBoundLower']
-wBoundUpper = config['wBoundUpper']
-boundLower = config['boundLower']
-boundUpper = config['boundUpper']
-mPatterns = config['mPatterns']
-nInputs = config['nInputs']
 
-if config['isRandom']:
-    inputs = np.random.uniform(
-        boundLower, boundUpper, size=(mPatterns, nInputs))
-    expectedOutput = np.random.uniform(boundLower, boundUpper, size=mPatterns)
+def generateInitialWeights(low, high, size):
+    return np.random.uniform(low, high, size=size)
 
-else:
-    patterns = np.loadtxt(config['path'], ndmin=2)
-    inputs = patterns[:, :-1]
-    expectedOutput = patterns[:, -1:].flatten()
 
-weights = np.random.uniform(wBoundLower, wBoundUpper, size=inputs.shape[1])
+def calculateOutput(inputs, weights):
+    output = 0
+    for i in range(len(inputs)):
+        output += inputs[i]*weights[i]
+    return output
 
-for i in range(epochs):
 
-    for j in range(inputs.shape[0]):
-        output = 0
-        for k in range(len(inputs[j])):
-            output += inputs[j][k]*weights[k]
+def calculateWeights(weights, trainingStep, inputs, output, expectedOutput):
+    for i in range(len(weights)):
+        weights[i] += trainingStep * (expectedOutput - output) * inputs[i]
 
-        for l in range(len(weights)):
-            weights[l] += trainingStep * \
-                (expectedOutput[j] - output) * inputs[j][l]
 
-print('Epochs: ', epochs)
-print('Training step: ', trainingStep)
-print('Final weights: ', weights)
+def main():
+    with open('config/config.json') as configFile:
+        config = json.load(configFile)
+
+    epochs = config['epochs']
+    trainingStep = config['trainingStep']
+    wBoundLower = config['wBoundLower']
+    wBoundUpper = config['wBoundUpper']
+    boundLower = config['boundLower']
+    boundUpper = config['boundUpper']
+    mPatterns = config['mPatterns']
+    nInputs = config['nInputs']
+
+    if config['isRandom']:
+        inputs = np.random.uniform(
+            boundLower, boundUpper, size=(mPatterns, nInputs))
+        expectedOutput = np.random.uniform(
+            boundLower, boundUpper, size=mPatterns)
+
+    else:
+        patterns = np.loadtxt(config['path'], ndmin=2)
+        inputs = patterns[:, :-1]
+        expectedOutput = patterns[:, -1:].flatten()
+
+    weights = generateInitialWeights(wBoundLower, wBoundUpper, inputs.shape[1])
+
+    error = []
+    for _ in range(epochs):
+        errorSum = 0
+        for i in range(inputs.shape[0]):
+            output = calculateOutput(inputs[i], weights)
+            errorSum += (expectedOutput[i] - output) ** 2
+            calculateWeights(
+                weights, trainingStep, inputs[i],  output, expectedOutput[i])
+        error.append(errorSum / inputs.shape[0])
+
+    print('Epochs: ', epochs)
+    print('Training step: ', trainingStep)
+    print('Final weights: ', weights)
+
+    sb.lineplot(data=error)
+    plt.title('Mean Square Error')
+    plt.ylabel('Error')
+    plt.xlabel('Epoch')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
